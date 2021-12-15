ccb5f56d59fcd2f379bdf3ae94f4842b1b655176
unknown <rubenvandenbrink@hotmail.com>
2021-12-15 17:23:18 +0100

    v1

---
+++ main.py
@@ -0,0 +1,160 @@
+import requests
+import os
+import hashlib
+import hmac
+import time
+
+# Your Twitter bearer token (https://developer.twitter.com/en/portal/dashboard).
+twitter_bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
+# User id you want to check tweets for.
+twitter_user_id = 44196397
+# Can be anything.
+twitter_user_agent = "ElonDogeBot"
+
+# Binance api key (https://www.binance.com/en/my/settings/api-management)
+binance_api_key = os.environ.get("BINANCE_API_KEY")
+# Binance api secret (https://www.binance.com/en/my/settings/api-management)
+binance_api_secret = os.environ.get("BINANCE_API_SECRET")
+
+# The trade pair you want to use
+trade_pair = "DOGEUSDT"
+# The quantity you want to trade in USDT
+trade_amount = "45"
+# The words that need to be tweeted in order to trigger a buy order
+keywords = ["DOGE"]
+
+timestamp = ""
+last_tweet = ""
+last_amount = 0
+
+
+def bearer_oauth(auth):
+    auth.headers["Authorization"] = f"Bearer {twitter_bearer_token}"
+    auth.headers["User-Agent"] = "ElonDogeListener"
+    return auth
+
+
+def get_tweets():
+    # The minimum of requesting tweets is 5, although we are only interested in the latest single tweet.
+    response = requests.get(
+        "https://api.twitter.com/2/users/{}/tweets?tweet.fields=created_at&exclude=replies&max_results=5".format(
+            twitter_user_id), auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception("Cannot get tweets (Code: {}) (Message: {})".format(response.status_code, response.text))
+    return response.json()
+
+
+def get_latest_tweet(json_data):
+    all_tweets = json_data.get('data')
+    latest_tweet = all_tweets[0]
+    latest_tweet_text = latest_tweet.get('text').upper()
+    print("latest tweet from user({}): {}".format(twitter_user_id, latest_tweet_text))
+    return latest_tweet_text
+
+
+def update_tweet_if_new(tweet):
+    global last_tweet
+    if tweet != last_tweet:
+        last_tweet = tweet
+        print("Latest tweet is new!")
+        return True
+    return False
+
+
+def check_if_tweet_matches_keywords():
+    for word in keywords:
+        if word.upper() in last_tweet:
+            print("{} matches criteria".format(word.upper()))
+            return True
+    return False
+
+
+def set_new_timestamp():
+    global timestamp
+    data = requests.get("https://api.binance.com/api/v3/time").json()
+    timestamp = data.get("serverTime")
+
+
+def generate_binance_signature(side):
+    return hmac.new(binance_api_secret.encode(),
+                    "symbol={}&side={}&type=MARKET&quantity={}&timestamp={}".format(trade_pair,
+                                                                                    side,
+                                                                                    last_amount,
+                                                                                    timestamp).encode(),
+                    hashlib.sha256).hexdigest()
+
+
+def create_order(side):
+    set_new_timestamp()
+    return requests.post("https://api.binance.com/api/v3/order",
+                         params={
+                             "symbol": trade_pair,
+                             "side": side,
+                             "type": "MARKET",
+                             "quantity": last_amount,
+                             "timestamp": timestamp,
+                             "signature": generate_binance_signature(side),
+                         },
+                         headers={
+                             "X-MBX-APIKEY": binance_api_key,
+                         })
+
+
+def create_buy_order():
+    response = create_order("BUY")
+    if response.status_code != 200:
+        raise Exception("Cannot make buy order (Code: {}) (Message: {})".format(response.status_code, response.text))
+    print("Buy order created for {} {}".format(last_amount, trade_pair))
+    return response.json()
+
+
+def create_sell_order():
+    response = create_order("SELL")
+    if response.status_code != 200:
+        raise Exception("Cannot make sell order (Code: {}) (Message: {})".format(response.status_code, response.text))
+    print("Sell order created for {} {}".format(last_amount, trade_pair))
+    return response.json()
+
+
+def calculate_trade_amount():
+    global last_amount
+    response = requests.get("https://api.binance.com/api/v3/ticker/price?symbol={}".format(trade_pair))
+    if response.status_code != 200:
+        raise Exception("Cannot get price (Code: {}) (Message: {})".format(response.status_code, response.text))
+    price = response.json().get("price")
+    amount = float(trade_amount) // float(price)
+    amount = int(amount)
+    print("Calculated amount for {} USDT: {} {}".format(trade_amount, amount, trade_pair))
+    last_amount = amount
+    return amount
+
+
+def calculate_sell_value():
+    response = requests.get("https://api.binance.com/api/v3/ticker/price?symbol={}".format(trade_pair))
+    if response.status_code != 200:
+        raise Exception("Cannot get price (Code: {}) (Message: {})".format(response.status_code, response.text))
+    print("Sell value: " + str(last_amount * float(response.json().get("price"))))
+
+
+def main():
+    global last_tweet
+    last_tweet = get_latest_tweet(get_tweets())
+
+    while True:
+        time.sleep(5)
+        tweets_response = get_tweets()
+        tweet = get_latest_tweet(tweets_response)
+        if not update_tweet_if_new(tweet):
+            continue
+        if not check_if_tweet_matches_keywords():
+            continue
+        calculate_trade_amount()
+        create_buy_order()
+        time.sleep(20)
+        create_sell_order()
+        calculate_sell_value()
+
+
+if __name__ == "__main__":
+    main()
